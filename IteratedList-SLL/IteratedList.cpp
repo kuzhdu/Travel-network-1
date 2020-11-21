ef13fbafdfdfdb9e6b99ddfc189a1260c07553f8
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2020-11-21 14:46:23 +0000

    New project structure

--- IteratedList-SLL/IteratedList.cpp
+++ IteratedList-SLL/IteratedList.cpp
@@ -107,4 +107,5 @@ void IteratedList::addToPosition(ListIterator &pos, TElem e) {
     this->it->first();
     while (this->it->valid())
     {
-        if (this->it->get
+        if (this->it->getCurrent() == e)
+         
