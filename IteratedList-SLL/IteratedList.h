25afa5c3c1de156ae50afc596aea27a006b7b5b9
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2021-03-30 08:17:57 +0000

    Log cleanup

--- IteratedList-SLL/IteratedList.h
+++ IteratedList-SLL/IteratedList.h
@@ -41,4 +41,5 @@ public:
 
     //adds a new element to a given position
     //after addition, pos points to the newly added element
-    //throws an exception if pos i
+    //throws an exception if pos is not valid
+    void a
