0fed40d974d144a86a6a89ef124581b7b901738b
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2021-04-27 06:57:24 +0000

    added folder

--- IteratedList-SLL/IteratedList.h
+++ IteratedList-SLL/IteratedList.h
@@ -46,4 +46,4 @@ public:
 
     //removes the element from position pos
     //returns the removed element
-    //after removal pos is positioned on the next element (the one after the removed one) or it is inval
+    //after removal pos is positioned on the next element (the one after the removed one) or it is invalid if the last element 
