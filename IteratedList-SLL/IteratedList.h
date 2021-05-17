9923288ae0183d899767055ef0fd05f7f0063bec
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2021-05-17 08:55:22 +0000

    Updated to latest API version.

--- IteratedList-SLL/IteratedList.h
+++ IteratedList-SLL/IteratedList.h
@@ -51,4 +51,4 @@ public:
     TElem remove(ListIterator& pos);
 
     // searches for the first occurrance of an element
-    //returns an iterator that points to the element, if it 
+    //returns an iterator that points to the element, if it appears in the list, or
