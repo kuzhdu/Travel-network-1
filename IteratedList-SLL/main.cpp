720e2ac0427bbc0f9baf222d64e165571de571a9
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2022-02-09 10:07:35 +0000

    More assertions

--- IteratedList-SLL/main.cpp
+++ IteratedList-SLL/main.cpp
@@ -8,3 +8,20 @@
 void testAllExtended2() {
     IteratedList list = IteratedList();
     assert(list.isEmpty());
+    ListIterator it0 = list.first();
+    assert(!it0.valid());
+    try {
+        it0.getCurrent();
+        assert(false);
+    }
+    catch (std::exception &) {
+        assert(true);
+    }
+
+    list.addToEnd(1);
+    assert(list.size() == 1);
+    ListIterator it = list.first();
+    assert(it.getCurrent() == 1);
+
+    ListIterator it2 = list.first();
+    for (int i = 0; i < 100; i++) {
