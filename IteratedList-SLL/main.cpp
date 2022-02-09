51172487c0e51a619875e49f0b18e0e0d71993f6
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2022-02-09 10:57:00 +0000

    Reformat for readability

--- IteratedList-SLL/main.cpp
+++ IteratedList-SLL/main.cpp
@@ -25,3 +25,27 @@ void testAllExtended2() {
 
     ListIterator it2 = list.first();
     for (int i = 0; i < 100; i++) {
+        list.addToEnd(i);
+        if (i <= 50) {
+            it2.next();
+        }
+        it.next();
+        assert(it.getCurrent() == i);
+    }
+    assert(it2.getCurrent() == 50);
+    assert(list.size() == 101);
+    assert(it.valid());
+    it.next();
+    assert(!it.valid());
+    try {
+        list.getElement(it);
+        assert(false);
+    }
+    catch (std::exception &) {
+        assert(true);
+    }
+
+    ListIterator it3 = list.search(50);
+    assert(it3.valid());
+    assert(it3.getCurrent() == 50);
+    TElem old = list.setElement(it3, 51);
