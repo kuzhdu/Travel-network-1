c40e7e9d67d0764fc2812d40ec2559213f2bc104
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2022-03-03 06:49:15 +0000

    Missing word

--- SortedMap-DLLA/DLLA.cpp
+++ SortedMap-DLLA/DLLA.cpp
@@ -11,4 +11,5 @@ DLLA::DLLA() {
     this->tail = -1;
     this->firstEmpty = 0;
     this->nodes = new DLLANode[1000];
-    for (int i = 0;i < this
+    for (int i = 0;i < this->capacity;i++)
+    
