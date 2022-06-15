3a2df204133e42ba54a5af6d570675c93e9d40ad
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2022-06-15 14:15:34 +0000

    WORKING

--- SortedMap-DLLA/DLLA.cpp
+++ SortedMap-DLLA/DLLA.cpp
@@ -109,4 +109,5 @@ void DLLA::insertElement(TElem elem, int position) {
             this->nodes[newPos].prev = nodC;
             this->nodes[nodC].next = newPos;
             if (nodNext == -1)
-                this->tail = newPos
+                this->tail = newPos;
+            els
