7058d23f8901fdd4debf580ad75a0665539fdb09
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2022-06-20 08:25:30 +0000

    src/Builder4/WindowState.vala

--- SortedMap-DLLA/DLLAIterator.cpp
+++ SortedMap-DLLA/DLLAIterator.cpp
@@ -11,3 +11,25 @@
 //Complexity: theta(1)
 DLLAIterator::DLLAIterator(DLLA &toIterate){
     this->toIterate = toIterate;
+    this->pos = toIterate.head;
+}
+
+//Complexity: theta(1)
+DLLAIterator::DLLAIterator(SortedMap &map){
+    this->toIterate = map.map;
+    this->pos = toIterate.head;
+}
+
+//Complexity: theta(1)
+void DLLAIterator::first() {
+    this->pos = this->toIterate.head;
+}
+//Complexity: theta(1)
+bool DLLAIterator::valid() {
+    if (this->pos == -1)
+        return false;
+    return true;
+}
+//Complexity: theta(1)
+void DLLAIterator::next() {
+    if (!this->valid())
