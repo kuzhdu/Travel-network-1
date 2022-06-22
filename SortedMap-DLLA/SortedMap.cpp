551dba5998ca398eacf24e07d140a6245131fead
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2022-06-22 14:14:02 +0000

    tcp: fix tcp_release_cb() to dispatch via address family for mtu_reduced()

--- SortedMap-DLLA/SortedMap.cpp
+++ SortedMap-DLLA/SortedMap.cpp
@@ -7,3 +7,26 @@
 #include "SortedMap.h"
 #include "unistd.h"
 #include <iostream>
+SortedMap::SortedMap(Relation r) {
+    this->r = r;
+}
+
+//Complexity O(n)
+TValue SortedMap::add(TKey c, TValue v) {
+    int index = this->map.head;
+    int position = 0;
+    int oldVal;
+    while (index != -1 && r(this->map.nodes[index].elem.first, c) == true)
+    {
+        position++;
+        //if the pair exists we replace the oldValue
+        if (this->map.nodes[index].elem.first == c)
+        {
+            oldVal = this->map.nodes[index].elem.second;
+            this->map.nodes[index].elem.second = v;
+            return oldVal;
+        }
+        index = this->map.nodes[index].next;
+    }
+    this->map.insertElement(TElem(c, v), position);
+    return NULL_TVALUE;
