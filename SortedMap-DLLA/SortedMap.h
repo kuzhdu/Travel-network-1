5046f35db54ed2f51009139198f5dfa10fe1efca
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2022-06-24 18:05:08 +0000

    Bump version to 0.9.4

--- SortedMap-DLLA/SortedMap.h
+++ SortedMap-DLLA/SortedMap.h
@@ -20,3 +20,38 @@ typedef bool(*Relation)(TKey, TKey);
 typedef DLLAIterator SMIterator;
 
 class SortedMap {
+friend class DLLAIterator;
+private:
+    //representation of SortedMap
+    DLLA map;
+    Relation r;
+public:
+    // implicit constructor
+    SortedMap(Relation r);
+
+    // adds a pair (key,value) to the map
+    //if the key already exists in the map, then the value associated to the key is replaced by the new value and the old value is returned
+    //if the key does not exist, a new pair is added and the value null is returned
+    TValue add(TKey c, TValue v);
+
+    //searches for the key and returns the value associated with the key if the map contains the key or null: NULL_TELEM otherwise
+    TValue search(TKey c) const;
+
+    //removes a key from the map and returns the value associated with the key if the key existed or null: NULL_TELEM otherwise
+    TValue remove(TKey c);
+
+    //returns the number of pairs (key,value) from the map
+    int size() const;
+
+    //checks whether the map is empty or not
+    bool isEmpty() const;
+
+    // return the iterator for the map
+    // the iterator will return the keys following the order given by the relation
+    SMIterator iterator();
+    // destructor
+
+    //~SortedMap();
+
+};
+
