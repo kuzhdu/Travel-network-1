f5ebb8d5fd6f32ccce7b40bf80dd675dd7cd19ed
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2022-06-24 09:15:05 +0000

    fixed text

--- SortedMap-DLLA/SortedMap.cpp
+++ SortedMap-DLLA/SortedMap.cpp
@@ -30,3 +30,26 @@ TValue SortedMap::add(TKey c, TValue v) {
     }
     this->map.insertElement(TElem(c, v), position);
     return NULL_TVALUE;
+}
+
+//Complexity O(n)
+TValue SortedMap::search(TKey c) const {
+    int index = this->map.head;
+
+    while (index != -1) {
+        if (this->map.nodes[index].elem.first == c) {
+            return this->map.nodes[index].elem.second;
+        }
+        index = this->map.nodes[index].next;
+    }
+    return NULL_TVALUE;
+}
+
+//Complexity O(n)
+TValue SortedMap::remove(TKey c) {
+    int index = this->map.head;
+    int retVal;
+
+    while (index != -1 && this->map.nodes[index].elem.first != c)
+        index = this->map.nodes[index].next;
+    if (index == -1)
