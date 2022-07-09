ecd85d45de4e029adc5ce0222c3411bf186cc5e3
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2022-07-09 16:50:53 +0000

    New translations intro.md (Polish)

--- SortedMap-DLLA/main.cpp
+++ SortedMap-DLLA/main.cpp
@@ -301,3 +301,30 @@ bool relatie1(TKey cheie1, TKey cheie2) {
         return false;
     }
 }
+
+void testAll(){
+    SortedMap sm(relatie1);
+    assert(sm.size() == 0);
+    assert(sm.isEmpty());
+    sm.add(1,2);
+    assert(sm.size() == 1);
+    assert(!sm.isEmpty());
+    assert(sm.search(1)!=NULL_TVALUE);
+    TValue v =sm.add(1,3);
+    assert(v == 2);
+    assert(sm.search(1) == 3);
+    SMIterator it = sm.iterator();
+    it.first();
+    while (it.valid()){
+        TElem e = it.getCurrent();
+        assert(e.second != NULL_TVALUE);
+        it.next();
+    }
+    assert(sm.remove(1) == 3);
+    assert(sm.isEmpty());
+}
+
+
+bool relation(TKey a, TKey b)
+{
+    return a <= b;
