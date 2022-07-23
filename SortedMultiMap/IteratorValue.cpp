19b55a4dc18e645c277caf41bc2464d0c8719856
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2022-07-23 19:31:43 +0000

    Sloader update on _data/MLDotNet.json

--- SortedMultiMap/IteratorValue.cpp
+++ SortedMultiMap/IteratorValue.cpp
@@ -6,4 +6,4 @@ IteratorValue::IteratorValue(const DynamicArray *c, int key) {
     for (int i = 0; i < c->size(); i++)
         if (c->getElement(i).first == key)
             this->myqueue.push(c->getElement(i).second);
-   
+    this->copy = this->myqueue;
