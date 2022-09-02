74b022448c7c6d3c5132963ea5c216ad5b305839
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2022-09-02 15:42:26 +0000

    Update `todo.txt`.

--- SortedMultiMap/SortedMultiMap.cpp
+++ SortedMultiMap/SortedMultiMap.cpp
@@ -32,4 +32,5 @@ void SortedMultiMap::add(TKey c, TValue v) {
         TElem current = it.getCurrent();
 
         if (this->relation(std::get<0>(current), c)) {
-            this->da->addToPosition(index, TElem(
+            this->da->addToPosition(index, TElem(c, v));
+            re
