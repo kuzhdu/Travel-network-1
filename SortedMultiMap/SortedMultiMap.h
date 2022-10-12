c26b1916b914e3307d23ab581bc80c4add886fd7
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2022-10-12 13:52:56 +0000

    First prototype

--- SortedMultiMap/SortedMultiMap.h
+++ SortedMultiMap/SortedMultiMap.h
@@ -15,3 +15,30 @@ typedef bool(*Relation)(TKey, TKey);
 
 class SortedMultiMap {
 private:
+    DynamicArray *da;
+    Relation relation;
+public:
+    // constructor
+    SortedMultiMap(Relation r);
+
+    //adds a new key value pair to the sorted multi map
+    void add(TKey c, TValue v);
+
+    //returns the values belonging to a given key
+    vector<TValue> search(TKey c) const;
+
+    //removes a key value pair from the sorted multimap
+    //returns true if the pair was removed (it was part of the multimap), false if nothing is removed
+    bool remove(TKey c, TValue v);
+
+    //returns the number of key-value pairs from the sorted multimap
+    int size() const;
+
+    //verifies if the sorted multi map is empty
+    bool isEmpty() const;
+
+    // returns an iterator for the sorted multimap. The iterator will returns the pairs as required by the relation (given to the constructor)
+    SMMIterator iterator() const;
+
+    // destructor
+    //~SortedMultiMap();
