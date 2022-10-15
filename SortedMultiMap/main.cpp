7c9c8d73763c6f0e6aa5c185d72d9379ce7c3c4e
kuzhdu <122413542+kuzhdu@users.noreply.github.com>
2022-10-15 08:49:42 +0000

    Update Readme.md.

--- SortedMultiMap/main.cpp
+++ SortedMultiMap/main.cpp
@@ -25,3 +25,27 @@ bool desc(TKey c1, TKey c2) {
 
 void testCreate() {
     SortedMultiMap smm = SortedMultiMap(asc);
+    assert(smm.size() == 0);
+    assert(smm.isEmpty());
+
+    for (int i = 0; i < 10; i++) {
+        vector<TValue> v= smm.search(i);
+        assert(v.size()==0);
+    }
+
+    for (int i = -10; i < 10; i++) {
+        vector<TValue> v= smm.search(i);
+        assert(v.size()==0);
+    }
+}
+
+void testSearch(Relation r) {
+    SortedMultiMap smm = SortedMultiMap(r);
+    int kMin = 0;
+    int kMax = 10;
+    for (int i = kMin; i <= kMax; i++) {
+        smm.add(i, i + 1);
+        smm.add(i, i + 2);
+    }
+    int intervalDim = 10;
+    for (int i = kMin; i <= kMax; i++) {
